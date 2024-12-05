7bc8ee97f39248e85f08d5dfdcc4a4f2593bba9a
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-05 12:09:11 -0800

    Start of Day commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import "./styles/styles.scss";
 import Hero from "./components/Hero";
-// import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
-      {/* <NavBar /> */}
       <Hero />
     </>
   );
