195a86c6326347ee18a6f41b3050fb8eaa4051a8
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-05 14:21:51 -0800

    Vanta integration

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,27 @@ import Hero from "./components/Hero";
 import MissionStatement from "./components/MissionStatement";
 
 function App() {
+  VANTA.FOG({
+    el: "#main-wrapper",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    highlightColor: 0x0,
+    midtoneColor: 0x161616,
+    lowlightColor: 0x141414,
+    baseColor: 0xc0c0c,
+    blurFactor: 0.88,
+    speed: 0.2,
+    zoom: 0.1,
+  });
+
   return (
-    <>
+    <main id="main-wrapper">
       <Hero />
       <MissionStatement />
-    </>
+    </main>
   );
 }
 
--- src/components/MissionStatement.jsx
+++ src/components/MissionStatement.jsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
 const MissionStatement = () => {
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    const text = textRef.current;
+
+    gsap.fromTo(
+      text,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        duration: 2,
+        text: {
+          value: text.innerHTML,
+          delimiter: "",
+        },
+        ease: "power1.inOut",
+      },
+    );
+  }, []);
+
   return (
     <section className="mission-statement">
-      <h2>My Mission</h2>
-      <p>
-        Crafting clean, responsive, and user-focused websites that bring ideas
-        to life with creativity and precision.
+      <p ref={textRef}>
+        Crafting <strong>clean, responsive, and user-focused</strong> websites
+        that bring ideas to life with
+        <span className="creativity">creativity</span> and{" "}
+        <span className="precision">precision</span>
       </p>
     </section>
   );
