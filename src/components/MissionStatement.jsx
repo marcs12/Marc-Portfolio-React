86aa6569649975a695efe475ca5b27007511989c
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-05 14:53:28 -0800

    Fixed Vanta background, useEffect

--- src/components/MissionStatement.jsx
+++ src/components/MissionStatement.jsx
@@ -2,29 +2,9 @@ import React, { useRef, useEffect } from "react";
 import { gsap } from "gsap";
 
 const MissionStatement = () => {
-  const textRef = useRef(null);
-
-  useEffect(() => {
-    const text = textRef.current;
-
-    gsap.fromTo(
-      text,
-      { opacity: 0 },
-      {
-        opacity: 1,
-        duration: 2,
-        text: {
-          value: text.innerHTML,
-          delimiter: "",
-        },
-        ease: "power1.inOut",
-      },
-    );
-  }, []);
-
   return (
     <section className="mission-statement">
-      <p ref={textRef}>
+      <p>
         Crafting <strong>clean, responsive, and user-focused</strong> websites
         that bring ideas to life with
         <span className="creativity">creativity</span> and{" "}
