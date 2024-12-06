cadc246d28170e7ff110e5cc06af380cb8d7b7ba
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-06 13:19:29 -0800

    Fixing Text Alignment on Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Links } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Projects = () => {
   return (
@@ -10,6 +10,7 @@ const Projects = () => {
             <p>01. </p>
             <h2>SnackLab</h2>
           </div>
+
           <hr />
         </li>
         <li>
