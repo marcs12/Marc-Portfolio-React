077323ef2a2616093e9c65453b97275220686704
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-12 18:44:18 -0800

    Fixing Social Icons Animation on Front Page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,13 @@ import "../styles/styles.scss";
 
 // Importing components
 import Hero from "./Hero";
+import SocialLinks from "./SocialLinks";
 
 function Home() {
   return (
     <main id="main-wrapper">
       <section id="hero">
+        <SocialLinks />
         <Hero />
       </section>
     </main>
