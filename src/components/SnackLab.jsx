85ffa8f2fbb44ea6561af8a486a399bee7324229
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-15 18:16:47 -0800

    Single Project Page, Hero done

--- src/components/SnackLab.jsx
+++ src/components/SnackLab.jsx
@@ -1,7 +1,55 @@
+import { section } from "framer-motion/client";
 import React from "react";
+import SnackLabVideo from "../assets/mockup-desktop/snacklab-mockups-4OBXVgqv.mp4";
 
 const SnackLab = () => {
-  return <div>SnackLab</div>;
+  return (
+    <section className="single-wrap">
+      <article className="single-hero-wrap">
+        <video src={SnackLabVideo} playsInline autoPlay muted loop></video>
+        <div className="heading-text-wrap">
+          <p>01.</p>
+          <h1>SnackLab</h1>
+        </div>
+        <div className="project-description">
+          <ul className="categories">
+            <li>
+              <p>Client:</p>
+            </li>
+            <li>
+              <p>Year:</p>
+            </li>
+            <li>
+              <p>Role:</p>
+            </li>
+            <li>
+              <p>Category:</p>
+            </li>
+            <li>
+              <p>Team:</p>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <p>Project</p>
+            </li>
+            <li>
+              <p>2024</p>
+            </li>
+            <li>
+              <p>Developer</p>
+            </li>
+            <li>
+              <p>E-Commerce</p>
+            </li>
+            <li>
+              <p>Marc Sapa, Gustavo Yamamoto, Kaleb Link, Haw Haw Tan</p>
+            </li>
+          </ul>
+        </div>
+      </article>
+    </section>
+  );
 };
 
 export default SnackLab;
