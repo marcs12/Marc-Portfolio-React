e10f3765b3cd6635ecee8083209e661bc3ef8614
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-17 13:12:45 -0800

    Starting About me Section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,30 +1,57 @@
-const About = () => {
-  return (
-    <section className="about-section">
-      <div className="about-me">
-        <h2>About Me</h2>
-        <p>
-          I am a full-stack web developer with a passion for creating clean,
-          responsive, and user-focused websites. I have experience with HTML,
-          CSS, JavaScript, React, Node.js, Express, MongoDB, and more. I am
-          always eager to learn new technologies and improve my skills.
-        </p>
-      </div>
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCss3Alt,
+  faHtml5,
+  faJs,
+  faReact,
+  faSass,
+  faWordpress,
+} from "@fortawesome/free-brands-svg-icons";
+import { faDatabase } from "@fortawesome/free-solid-svg-icons";
 
-      <div className="skills">
-        <h2>Skills</h2>
-        <ul>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>JavaScript</li>
-          <li>React</li>
-          <li>Node.js</li>
-          <li>Express</li>
-          <li>MongoDB</li>
-        </ul>
+const AboutMe = () => {
+  return (
+    <div className="about-me">
+      <div className="profile-container">
+        <div className="image-placeholder">
+          <img src="" alt="Profile Placeholder" />
+        </div>
+        <div className="description">
+          <p>
+            Marc Sapa is a multidisciplinary creative with a focus on creating
+            high-level work across a variety of digital mediums such as
+            automotive visualization and design.
+          </p>
+        </div>
       </div>
-    </section>
+      <div className="skills-container">
+        <div className="skills">
+          <div className="skill-icon">
+            <FontAwesomeIcon icon={faReact} />
+          </div>
+          <div className="skill-icon">
+            <FontAwesomeIcon icon={faHtml5} />
+          </div>
+          <div className="skill-icon">
+            <FontAwesomeIcon icon={faCss3Alt} />
+          </div>
+          <div className="skill-icon">
+            <FontAwesomeIcon icon={faJs} />
+          </div>
+          <div className="skill-icon">
+            <FontAwesomeIcon icon={faDatabase} />
+          </div>
+          <div className="skill-icon">
+            <FontAwesomeIcon icon={faSass} />
+          </div>
+          <div className="skill-icon">
+            <FontAwesomeIcon icon={faWordpress} />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
-export default About;
+export default AboutMe;
