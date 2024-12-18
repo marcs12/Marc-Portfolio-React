ab25db69bda9137bcafd2148582d4d276cf47793
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-17 16:03:43 -0800

    added a try/catch for the animated background incase

--- src/App.jsx
+++ src/App.jsx
@@ -27,21 +27,25 @@ function App() {
   }, []);
 
   useEffect(() => {
-    VANTA.FOG({
-      el: "html",
-      mouseControls: true,
-      touchControls: true,
-      gyroControls: false,
-      minHeight: 200.0,
-      minWidth: 200.0,
-      highlightColor: 0x141414,
-      midtoneColor: 0x111111,
-      lowlightColor: 0x111111,
-      baseColor: 0xc0c0c,
-      blurFactor: 0.9,
-      speed: 1.4,
-      zoom: 0.2,
-    });
+    try {
+      VANTA.FOG({
+        el: "html",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.0,
+        minWidth: 200.0,
+        highlightColor: 0x141414,
+        midtoneColor: 0x111111,
+        lowlightColor: 0x111111,
+        baseColor: 0xc0c0c,
+        blurFactor: 0.9,
+        speed: 1.4,
+        zoom: 0.2,
+      });
+    } catch (e) {
+      document.body.style.backgroundColor = "#0b0b0b";
+    }
   }, []);
 
   return (
