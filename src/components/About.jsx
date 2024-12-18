6e50fbbaaab67d624997d598b1209d4b483b2662
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-18 15:18:48 -0800

    Top Section About Done

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import Barcode from "../assets/barcode-long.png";
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import MarcPhoto from "../assets/IMG_1968.JPG";
 
 const About = () => {
+  const sectionRef = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      sectionRef.current,
+      { height: "0", opacity: 0 },
+      { height: "100%", opacity: 1, duration: 3 },
+    );
+  }, []);
+
   return (
-    <section className="about-section">
+    <section className="about-section" ref={sectionRef}>
       <article className="top-section">
         <img
           className="barcode-ornament"
@@ -12,6 +25,14 @@ const About = () => {
           aria-hidden="true"
         />
         <div className="vl"></div>
+        <div className="photo-intro">
+          <img className="marc-photo" src={MarcPhoto} alt="Marc Sapa" />
+          <p className="introduction">
+            Marc Sapa is a multidisciplinary creative with a focus on creating
+            high-level work across a variety of digital mediums such as
+            automotive visualization and design.
+          </p>
+        </div>
       </article>
     </section>
   );
