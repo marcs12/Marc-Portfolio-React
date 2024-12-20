42f40eb036106ed8831fd8c38d7da48ddccaf3d3
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-20 01:57:29 -0800

    Debug API Time

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // React and hooks
 import { useEffect } from "react";
-
+import VANTA from "vanta";
+import { Fog } from "vanta/dist/vanta.fog.min.js";
 // React Router
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
