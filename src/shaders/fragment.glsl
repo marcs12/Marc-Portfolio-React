4e29e63b2e2f8818b610c0ad0ee7bbe12a6d5b1f
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-20 14:26:34 -0800

    Safety Commit - Updating WebGL to Debug

--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,20 +1,15 @@
-// fragment.glsl
-
 uniform sampler2D uTexture;
 uniform float uAlpha;
 uniform vec2 uOffset;
 varying vec2 vUv;
 
+vec3 rgbShift(sampler2D textureimage, vec2 uv, vec2 offset) {
+    float r = texture2D(textureimage, uv + offset).r;
+    vec2 gb = texture2D(textureimage, uv).gb;
+    return vec3(r, gb);
+}
+
 void main() {
-    vec2 uvR = vUv + uOffset * 0.05;
-    vec2 uvG = vUv;
-    vec2 uvB = vUv - uOffset * 0.05;
-
-    float r = texture2D(uTexture, uvR).r;
-    float g = texture2D(uTexture, uvG).g;
-    float b = texture2D(uTexture, uvB).b;
-
-    vec4 color = vec4(r, g, b, uAlpha);
-
-    gl_FragColor = color;
+    vec3 color = rgbShift(uTexture, vUv, uOffset);
+    gl_FragColor = vec4(color, uAlpha);
 }
--- src/shaders/vertex.glsl
+++ src/shaders/vertex.glsl
@@ -1,12 +1,17 @@
-// vertex.glsl
-
+uniform sampler2D uTexture;
 uniform vec2 uOffset;
 varying vec2 vUv;
 
+float M_PI = 3.1415926535897932384626433832795;
+
+vec3 deformationCurve(vec3 position, vec2 uv, vec2 offset) {
+    position.x = position.x + (sin(uv.y * M_PI) * offset.x);
+    position.y = position.y + (sin(uv.x * M_PI) * offset.y);
+    return position;
+}
+
 void main() {
     vUv = uv;
-    vec3 newPosition = position;
-    newPosition.x += uOffset.x;
-    newPosition.y += uOffset.y;
+    vec3 newPosition = deformationCurve(position, uv, uOffset);
     gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
 }
