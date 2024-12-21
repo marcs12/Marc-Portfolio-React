94b8f3b2eacf7df76009d547410ffedd94bbb968
Edmarc Sapa <marcgsapa@gmail.com>
2024-12-20 19:57:34 -0800

    Loading Screen added and fixed time zone

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-// React and hooks
-import { useEffect } from "react";
-// import VANTA from "vanta";
-// import * as THREE from "three";
-
-// React Router
+import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
-// GSAP for animations
 import { gsap } from "gsap";
-
-// Components
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import About from "./components/About";
@@ -19,19 +10,17 @@ import ThirtyFive from "./components/ThirtyFive";
 import RunYuji from "./components/RunYuji";
 import Portfolio from "./components/Portfolio";
 import CursorTrail from "./components/CursorTrail";
-import PageTransition from "./components/PageTransition"; // Import PageTransition
-
+import PageTransition from "./components/PageTransition";
 import NotFound from "./components/NotFound";
 import ScrollToTop from "./components/ScrollToTop";
+import LoadingScreen from "./components/LoadingScreen"; // Import LoadingScreen
 
 function App() {
-  useEffect(() => {
-    gsap.fromTo(
-      ".slide-tabs",
-      { opacity: 0, width: 0 },
-      { opacity: 1, width: "auto", delay: 1, duration: 1 },
-    );
-  }, []);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleLoadingFinish = () => {
+    setIsLoading(false);
+  };
 
   useEffect(() => {
     try {
@@ -58,77 +47,83 @@ function App() {
   const isLargeScreen = window.innerWidth > 1024;
 
   return (
-    <Router>
-      {isLargeScreen && <CursorTrail />}
-      <ScrollToTop />
-      <NavBar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <PageTransition>
-              <Home />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/works"
-          element={
-            <PageTransition>
-              <Projects />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <PageTransition>
-              <About />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/works/snacklab"
-          element={
-            <PageTransition>
-              <SnackLab />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/works/thirtyfive"
-          element={
-            <PageTransition>
-              <ThirtyFive />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/works/runyuji"
-          element={
-            <PageTransition>
-              <RunYuji />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="/works/portfolio"
-          element={
-            <PageTransition>
-              <Portfolio />
-            </PageTransition>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <PageTransition>
-              <NotFound />
-            </PageTransition>
-          }
-        />
-      </Routes>
-    </Router>
+    <>
+      {isLoading ? (
+        <LoadingScreen onFinish={handleLoadingFinish} />
+      ) : (
+        <Router>
+          {isLargeScreen && <CursorTrail />}
+          <ScrollToTop />
+          <NavBar />
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <PageTransition>
+                  <Home />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/works"
+              element={
+                <PageTransition>
+                  <Projects />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/about"
+              element={
+                <PageTransition>
+                  <About />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/works/snacklab"
+              element={
+                <PageTransition>
+                  <SnackLab />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/works/thirtyfive"
+              element={
+                <PageTransition>
+                  <ThirtyFive />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/works/runyuji"
+              element={
+                <PageTransition>
+                  <RunYuji />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="/works/portfolio"
+              element={
+                <PageTransition>
+                  <Portfolio />
+                </PageTransition>
+              }
+            />
+            <Route
+              path="*"
+              element={
+                <PageTransition>
+                  <NotFound />
+                </PageTransition>
+              }
+            />
+          </Routes>
+        </Router>
+      )}
+    </>
   );
 }
 
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -31,6 +31,7 @@ const Hero = () => {
       const formattedTime = date.toLocaleTimeString("en-US", {
         hour: "2-digit",
         minute: "2-digit",
+        second: "2-digit",
         hour12: true,
         timeZoneName: "short",
       });
